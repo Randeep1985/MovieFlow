b312ffb61ff60dd44f2360e0b884346fbc9d0095
Randeep1985 <randeep.sk.1985@gmail.com>
2024-11-04 17:57:21 +0100

    layout page

--- src/(auth)/layout.tsx
+++ src/(auth)/layout.tsx
@@ -1,8 +1,26 @@
+import { useAuthStore } from '@/store/Auth'
+import { useRouter } from 'next/router'
 import React from 'react'
 
 const layout = ({children}: {children: React.ReactNode}) => {
  
+    const {session} = useAuthStore()
+    const router = useRouter()
+
+    React.useEffect(() => {
+        if (session) router.push('/')
+    }, [session, router])
+
+    if (session) {
+        return null
+    }
     
+    return (
+        <div>
+            {children}
+        </div>
+    )
+
 }
 
 export default layout
